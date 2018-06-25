be001147c07fd462143f64f457d22778cae6a15c
Miguel Torres <migueltg93@gmail.com>
2018-06-25 12:12:45 -0500

    installed firebase

---
+++ src/firebase/init.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBv4DpG4dS9D_IRKiyzVaFCeEy14f5nN8k",
+  authDomain: "ninja-smoothies-76937.firebaseapp.com",
+  databaseURL: "https://ninja-smoothies-76937.firebaseio.com",
+  projectId: "ninja-smoothies-76937",
+  storageBucket: "ninja-smoothies-76937.appspot.com",
+  messagingSenderId: "396685692753"
+};
+const firebaseApp = firebase.initializeApp(config);
+firebaseApp.firestore().settings({ timestampsInSnapshots = true })
+
+// export firestore databaseURL
+export default firebaseApp.firestore()
